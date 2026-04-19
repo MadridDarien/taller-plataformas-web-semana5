3185859ebb344ce678f506ac5f78af4746977e2c
Diego <diego.barahona.b@gmail.com>
2026-04-18 23:18:26 -0300

    agrega ruta /home

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
     res.send("Servidor funcionando correctamente");
 });
 
+router.get("/home", (req, res) => {
+  res.send("Bienvenido al Home de AIEP, el trabajo de la semana 5");
+});
+
 module.exports = router;
